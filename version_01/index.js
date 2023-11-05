fc973cacdd1d251f99531dadd5b61619f582cf12
salinpaul valooran <salinpv@hotmail.com>
2023-11-05 22:51:33 +0530

    window function to select changed voice

--- version_01/index.js
+++ version_01/index.js
@@ -2,6 +2,9 @@ let speech = new SpeechSynthesisUtterance();
 let voices = [];
 
 let voicesSelect = document.querySelector("select");
+window.speechSynthesis.onvoiceschanged = () => {
+  
+}
 
 document.querySelector("button").addEventListener("click", () => {
   speech.text = document.querySelector("textarea").value;
