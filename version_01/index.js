6c30e9a33374beab4e4c80bffee0ccfdd299e7bc
salinpaul valooran <salinpv@hotmail.com>
2023-11-05 22:50:04 +0530

    voices array variable

--- version_01/index.js
+++ version_01/index.js
@@ -1,4 +1,5 @@
 let speech = new SpeechSynthesisUtterance();
+let voices = [];
 
 document.querySelector("button").addEventListener("click", () => {
   speech.text = document.querySelector("textarea").value;
