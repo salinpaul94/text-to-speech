bfb6be5ff1f76e5a44b49f331ffea427e73b6a21
salinpaul valooran <salinpv@hotmail.com>
2023-11-05 22:50:48 +0530

    variable for voice selector

--- version_01/index.js
+++ version_01/index.js
@@ -1,6 +1,8 @@
 let speech = new SpeechSynthesisUtterance();
 let voices = [];
 
+let voicesSelect = document.querySelector("select");
+
 document.querySelector("button").addEventListener("click", () => {
   speech.text = document.querySelector("textarea").value;
   window.speechSynthesis.speak(speech);
