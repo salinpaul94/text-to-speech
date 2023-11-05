4b61cd478d5a9d8976db16d2dde4705eb274b91c
salinpaul valooran <salinpv@hotmail.com>
2023-11-05 23:03:28 +0530

    event listener on voice select

--- version_01/index.js
+++ version_01/index.js
@@ -7,7 +7,11 @@ window.speechSynthesis.onvoiceschanged = () => {
   speech.voice = voices[0];
 
   voices.forEach((voice, i) => (voicesSelect.options[i] = new Option(voice.name, i)));
-}
+};
+
+voicesSelect.addEventListener("change", () => {
+  speech.voice = voices[voicesSelect.value];
+})
 
 document.querySelector("button").addEventListener("click", () => {
   speech.text = document.querySelector("textarea").value;
